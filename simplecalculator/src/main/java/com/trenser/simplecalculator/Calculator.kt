bb9fdc762dd01fe20399d4de657bb3b03673f4a9
Bijin Baby <bijin.baby@trenser.com>
2019-08-26 14:14:36 +0530

    bnbm

---
+++ simplecalculator/src/main/java/com/trenser/simplecalculator/Calculator.kt
@@ -0,0 +1,24 @@
+package com.trenser.simplecalculator
+
+object Calculator {
+
+    fun sum(a:Int ,b: Int):Int{
+        return a+b
+    }
+
+    fun diff(a :Int, b: Int):Int{
+        return a-b
+    }
+
+    fun mul(a:Int,b:Int):Int{
+        return a * b
+    }
+
+    fun div(a:Int,b:Int):Int{
+        return a / b
+    }
+
+    fun mod(a:Int,b:Int):Int{
+        return a % b
+    }
+}
